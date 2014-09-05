81c861a465394eb06adf20934d84122808336ae4
podcastqq <podcast.qq@gmail.com>
2014-09-05 09:32:47 +0300

    crud

---
+++ get_user_details.php
@@ -0,0 +1,71 @@
+<?php
+ 
+/*
+ * Following code will get single user details
+ * A user is identified by product id (pid)
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+ 
+// connecting to db
+$db = new DB_CONNECT();
+ 
+// check for post data
+if (isset($_GET["pid"])) {
+    $pid = $_GET['pid'];
+ 
+    // get a user from admin_users table
+    $result = mysql_query("SELECT *FROM admin_user WHERE pid = $pid");
+ 
+    if (!empty($result)) {
+        // check for empty result
+        if (mysql_num_rows($result) > 0) {
+ 
+            $result = mysql_fetch_array($result);
+ 
+            $user = array();
+            $user["pid"] = $result["pid"];
+            $user["name"] = $result["name"];
+            $user["username"] = $result["username"];
+            $user["surname"] = $result["surname"];
+            $user["pass"] = $result["pass"];
+            
+            // success
+            $response["success"] = 1;
+ 
+            // user node
+            $response["user"] = array();
+ 
+            array_push($response["user"], $user);
+ 
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // no user found
+            $response["success"] = 0;
+            $response["message"] = "No user found";
+ 
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    } else {
+        // no user found
+        $response["success"] = 0;
+        $response["message"] = "No user found";
+ 
+        // echo no users JSON
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
